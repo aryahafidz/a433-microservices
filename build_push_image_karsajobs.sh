a408b2ec67d8b9db141b8e7513ad15cdc40a8ec0
“Arya <aryahafidz345@gmail.com>
2023-11-14 12:38:54 +0700

    first commit

---
+++ build_push_image_karsajobs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Build Docker image
+docker build -t aryahafidz/karsajobs:latest .
+
+# Login ke Docker Hub
+docker login --username aryahafidz --password dckr_pat_Dc3PAGNIaa1PujyU-OmVDLrqga8
+
+# Push image ke Docker Hub
+docker push aryahafidz/karsajobs:latest
