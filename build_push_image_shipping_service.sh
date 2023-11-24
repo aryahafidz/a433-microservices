9f483e19ec4e69b12d34ae03cb39f94bb590e69d
“Arya <aryahafidz345@gmail.com>
2023-11-24 15:00:52 +0700

    commit docker compose

--- build_push_image_shipping_service.sh
+++ build_push_image_shipping_service.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Build Docker image
-docker build -t aryahafidz/shipping-service .
+docker build -t ghcr.io/aryahafidz/shipping-service:latest .
 
 # Login ke Docker Hub
-docker login --username aryahafidz --password dckr_pat_Dc3PAGNIaa1PujyU-OmVDLrqga8
+echo "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" | docker login ghcr.io -u aryahafidz --password-stdin
 
 # Push image ke Docker Hub
-docker push aryahafidz/shipping-service
+docker push ghcr.io/aryahafidz/shipping-service:latest
